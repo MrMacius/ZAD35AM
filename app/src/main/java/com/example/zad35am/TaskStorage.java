a2156fb427a575b6f81e280eb1a96b722b3785a0
Łukasz Leończak <82775@student.pb.edu.pl>
2023-11-28 06:20:00 +0100

    clip-arty

--- app/src/main/java/com/example/zad35am/TaskStorage.java
+++ app/src/main/java/com/example/zad35am/TaskStorage.java
@@ -17,7 +17,11 @@ public class TaskStorage {
             task.setName("PILNE ZADANIE NR"+i);
             task.setDone(i%3==0);
             tasks.add(task);
-
+            if(i%3 == 0){
+                task.setCategory(Category.STUDIES);
+            }else{
+                task.setCategory(Category.HOME);
+            }
         }
 
     }
